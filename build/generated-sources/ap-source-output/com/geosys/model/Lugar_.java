c424413e804f2a447ef3aad76c7a1419a11b126b
walter <geosys@tesis.com>
2014-10-16 22:28:29 -0500

    ESTA GENERADO PARA QUE USUARIOS NO LOGEADOS PUEDAN INGRESAR AL SISTEMA

--- build/generated-sources/ap-source-output/com/geosys/model/Lugar_.java
+++ build/generated-sources/ap-source-output/com/geosys/model/Lugar_.java
@@ -7,7 +7,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.0.v20130507-rNA", date="2014-05-29T05:18:32")
+@Generated(value="EclipseLink-2.5.0.v20130507-rNA", date="2014-10-11T15:18:32")
 @StaticMetamodel(Lugar.class)
 public class Lugar_ { 
 
--- build/generated-sources/ap-source-output/com/geosys/model/Sugerencias_.java
+++ build/generated-sources/ap-source-output/com/geosys/model/Sugerencias_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.0.v20130507-rNA", date="2014-05-29T05:18:32")
+@Generated(value="EclipseLink-2.5.0.v20130507-rNA", date="2014-10-11T15:18:32")
 @StaticMetamodel(Sugerencias.class)
 public class Sugerencias_ { 
 
